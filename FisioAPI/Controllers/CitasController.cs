1b18b2adc143ef8391934806280e5de773a81117
Santiago Bravo <67444457+sbravo15@users.noreply.github.com>
2023-02-08 19:55:26 -0600

    Model / Controller Citas API
    Model / Controller para Citas API

---
+++ FisioAPI/Controllers/CitasController.cs
@@ -0,0 +1,119 @@
+﻿using FisioAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+
+// 1.  Registrar Cita, 2 Editar Citas , 3. Consultar Cita Doctor, 4. Consultar ID Citas, 5 . Consultar Todas las Citas
+
+namespace FisioAPI.Controllers
+{
+    public class CitasController : ApiController
+    {
+        CitasModel instanciaCitas = new CitasModel();
+        BitacoraModel instanciaBitacora = new BitacoraModel();
+
+        //Registrar Cita
+ 
+        [HttpPost]
+        [Route("api/Citas/Registrar_Cita")]
+        public RespuestaCitasObj Registrar_Cita(CitasObj cita)
+        {
+            try
+            {
+                return instanciaCitas.Registrar_Cita(cita);
+            }
+            catch (Exception ex)
+            {
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+                respuesta.Codigo = -1;
+                respuesta.Mensaje = "Se presentó un error inesperado";
+                return respuesta;
+            }
+        }
+
+        // Editar Cita
+ 
+        [HttpPost]
+        [Route("api/Citas/Editar_Cita")]
+        public RespuestaCitasObj Editar_Cita(CitasObj cita)
+        {
+            try
+            {
+                return instanciaCitas.Editar_Cita(cita);
+            }
+            catch (Exception ex)
+            {
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+                respuesta.Codigo = -1;
+                respuesta.Mensaje = "Se presentó un error inesperado";
+                return respuesta;
+            }
+        }
+
+        //Consultar Cita Doctor
+
+        [HttpGet]
+        [Route("api/Citas/Consultar_Citas_Doctor")]
+        public RespuestaCitasObj Consultar_Citas_Doctor(int idDoctor)
+        {
+            try
+            {
+                return instanciaCitas.Consultar_Cita_Doctor(idDoctor);
+            }
+            catch (Exception ex)
+            {
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+                respuesta.Codigo = -1;
+                respuesta.Mensaje = "Se presentó un error inesperado";
+                return respuesta;
+            }
+        }
+
+        // Consultar ID Citas Paciente
+
+
+        [HttpGet]
+        [Route("api/Citas/Consultar_Citas_Paciente")]
+        public RespuestaCitasObj Consultar_Consultar_IdCita(int idPaciente)
+        {
+            try
+            {
+                return instanciaCitas.Consultar_IdCita(idPaciente);
+            }
+            catch (Exception ex)
+            {
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+                respuesta.Codigo = -1;
+                respuesta.Mensaje = "Se presentó un error inesperado";
+                return respuesta;
+            }
+
+        }
+
+        // Consultar ID Citas Paciente
+
+        [HttpGet]
+        [Route("api/Citas/Consultar_Citas")]
+        public RespuestaCitasObj Consultar_Citas()
+        {
+            try
+            {
+                return instanciaCitas.Consultar_Citas();
+            }
+            catch (Exception ex)
+            {
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+                respuesta.Codigo = -1;
+                respuesta.Mensaje = "Se presentó un error inesperado";
+                return respuesta;
+            }
+        }
+        
+
+
+    }
+}
---
+++ FisioAPI/Models/CitasModel.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using FisioAPI.Database;
+using System.IdentityModel.Tokens;
+using SymmetricSecurityKey = Microsoft.IdentityModel.Tokens.SymmetricSecurityKey;
+using SigningCredentials = Microsoft.IdentityModel.Tokens.SigningCredentials;
+using System.CodeDom;
+
+// 1.  Registrar Cita, 2 Editar Citas , 3. Consultar Cita Doctor, 4. Consultar ID Citas, 5 . Consultar Todas las Citas
+namespace FisioAPI.Models
+{
+    public class CitasModel
+    {
+        public RespuestaCitasObj Registrar_Cita(CitasObj cita)   // 1.  Registrar Cita
+        {
+            using (var con = new MOSSAEntities())
+            {
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+
+                var resultado = con.Registrar_Cita(cita.IdUsuario,cita.IdEmpleado,cita.Condicion, cita.Hora);
+
+               
+
+                if (resultado > 0)
+                {
+                    respuesta.Codigo = 1;
+                    respuesta.Mensaje = "Ok";
+                }
+                else
+                {
+                    respuesta.Codigo = 0;
+                    respuesta.Mensaje = "No se realizó la transacción";
+                }
+
+                return respuesta;
+            }
+        }
+
+
+        public RespuestaCitasObj Editar_Cita(CitasObj cita)  // 2 Editar Citas 
+        {
+            using (var con = new MOSSAEntities())
+            {
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+
+                var resultado = con.Editar_Citas(cita.IdCita,cita.IdUsuario,cita.IdEmpleado,cita.Condicion,cita.Hora,cita.Status);
+
+                if (resultado > 0)
+                {
+                    respuesta.Codigo = 1;
+                    respuesta.Mensaje = "Ok";
+                }
+                else
+                {
+                    respuesta.Codigo = 0;
+                    respuesta.Mensaje = "No se realizó la transacción";
+                }
+
+                return respuesta;
+            }
+        }
+
+
+        public RespuestaCitasObj Consultar_Cita_Doctor(int idDoctor)   // 3. Consultar Cita Doctor 
+        {
+            using (var con = new MOSSAEntities())
+            {
+                var resultado = con.Consultar_Citas_Doctor(idDoctor).ToList();
+
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+
+                if (resultado.Count > 0)
+                {
+                    List<CitasObj> datos = new List<CitasObj>();
+
+                    foreach (var item in resultado)
+                    {
+                        datos.Add(new CitasObj
+                        {
+                            IdCita = item.IdCitas,
+                            IdUsuario = item.IdUsuarioFk,
+                            IdEmpleado = item.IdDoctorFK,
+                            Condicion = item.condicion,
+                            Hora = item.Hora,
+                            Status = item.status
+                        });
+                    }
+
+                    respuesta.Codigo = 1;
+                    respuesta.Mensaje = "Ok";
+                    respuesta.lista = datos;
+                }
+                else
+                {
+                    respuesta.Codigo = 0;
+                    respuesta.Mensaje = "No se encontraron resultados";
+                }
+
+                return respuesta;
+            }
+        }
+
+
+        public RespuestaCitasObj Consultar_IdCita(int idPaciente)    // 4. Consultar ID Citas
+        {
+            using (var con = new MOSSAEntities())
+            {
+                var resultado = con.Consultar_Citas_Paciente(idPaciente).ToList();
+
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+
+                if (resultado.Count > 0)
+                {
+                    List<CitasObj> datos = new List<CitasObj>();
+
+                    foreach (var item in resultado)
+                    {
+                        datos.Add(new CitasObj
+                        {
+                            IdCita = item.IdCitas,
+                            IdUsuario = item.IdUsuarioFk,
+                            IdEmpleado = item.IdDoctorFK,
+                            Condicion = item.condicion,
+                            Hora = item.Hora,
+                            Status = item.status
+                        });
+                    }
+
+                    respuesta.Codigo = 1;
+                    respuesta.Mensaje = "Ok";
+                    respuesta.lista = datos;
+                }
+                else
+                {
+                    respuesta.Codigo = 0;
+                    respuesta.Mensaje = "No se encontraron resultados";
+                }
+
+                return respuesta;
+            }
+        }
+
+
+        public RespuestaCitasObj Consultar_Citas()   // 5 . Consultar Todas las Citas
+        {
+            using (var con = new MOSSAEntities())
+            {
+                var resultado = new List<Citas>();
+
+                resultado = (from x in con.Citas
+                             select x).ToList();
+
+                RespuestaCitasObj respuesta = new RespuestaCitasObj();
+
+                if (resultado.Count > 0)
+                {
+                    List<CitasObj> datos = new List<CitasObj>();
+
+                    foreach (var item in resultado)
+                    {
+                        datos.Add(new CitasObj
+                        {
+                            IdCita = item.IdCitas,
+                            IdUsuario = item.IdUsuarioFk,
+                            IdEmpleado = item.IdDoctorFK,
+                            Condicion = item.condicion,
+                            Hora = item.Hora,
+                            Status = item.status
+                        });
+                    }
+                    respuesta.Codigo = 1;
+                    respuesta.Mensaje = "Ok";
+                    respuesta.lista = datos;
+                }
+                else
+                {
+                    respuesta.Codigo = 0;
+                    respuesta.Mensaje = "No se encontraron resultados";
+                }
+                return respuesta;
+            }
+
+        }
+
+    }
+}
